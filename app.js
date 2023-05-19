54736a47b24c706378b06036d44f4cb6923fb6db
kinglearned <kinglearned@gmail.com>
2023-05-19 09:57:53 +0100

    Finalizing the Module

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const app = express()
 const PORT = 3300
 
 const Pwd = 'emmilia&811# 3697821'
-const word = stringCrypt.encrypt(Pwd)
+const word = stringCrypt.compare('emla81 672mii&1#3981',Pwd)
 
 console.log(word)
 
-app.listen(PORT, () =>  { console.log(`app running on PORT: ${PORT}`)})
+app.listen(PORT, () =>  { console.log()})
